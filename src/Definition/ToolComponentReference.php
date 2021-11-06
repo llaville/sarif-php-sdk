2f576940031db54209a003799ee211c91290c8d9
Laurent Laville <laurent.laville@gmail.com>
2021-11-06 09:10:06 +0000

    add the toolComponentReference object

---
+++ src/Definition/ToolComponentReference.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Sarif-PHP-SDK package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Bartlett\Sarif\Definition;
+
+use Bartlett\Sarif\Internal\JsonSerializable;
+use Bartlett\Sarif\Property\Guid;
+use Bartlett\Sarif\Property\Index;
+use Bartlett\Sarif\Property\Name;
+use Bartlett\Sarif\Property\Properties;
+
+/**
+ * Identifies a particular toolComponent object, either the driver or an extension.
+ *
+ * @author Laurent Laville
+ */
+final class ToolComponentReference extends JsonSerializable
+{
+    /**
+     * The 'name' property of the referenced toolComponent.
+     */
+    use Name;
+
+    /**
+     * An index into the referenced toolComponent in tool.extensions.
+     */
+    use Index;
+
+    /**
+     * The 'guid' property of the referenced toolComponent.
+     */
+    use Guid;
+
+    /**
+     * Key/value pairs that provide additional information about the toolComponentReference.
+     */
+    use Properties;
+
+    public function __construct()
+    {
+        $this->required = [];
+        $this->optional = [
+            'name',
+            'index',
+            'guid',
+            'properties',
+        ];
+    }
+}
