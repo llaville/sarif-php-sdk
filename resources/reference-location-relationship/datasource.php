e90b2e85ff00f0689e4316ab85ab1a1f5c0764c1
Laurent Laville <laurent.laville@gmail.com>
2024-01-02 13:16:38 +0000

    upgrade resources to generate UML graphes of all Reference's objects

---
+++ resources/reference-location-relationship/datasource.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Sarif-PHP-SDK package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Laurent Laville
+ * @since Release 1.1.0
+ */
+
+return function (): Generator {
+    $classes = [
+        \Bartlett\Sarif\Definition\LocationRelationship::class,
+    ];
+    foreach ($classes as $class) {
+        yield $class;
+    }
+};
---
+++ resources/reference-sarif-log/options.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Sarif-PHP-SDK package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Laurent Laville
+ * @since Release 1.1.0
+ */
+
+return [
+    // @link https://graphviz.gitlab.io/docs/attrs/rankdir/
+    'graph.rankdir' => 'TB',
+    // @link https://plantuml.com/en/color
+    'cluster.Bartlett\Sarif.graph.bgcolor' => 'BurlyWood',
+    'cluster.Bartlett\Sarif\Internal.graph.bgcolor' => 'Bisque',
+];
