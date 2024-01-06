b61519e3b1e12b248a49a4c29d90b52483a679c4
Laurent Laville <laurent.laville@gmail.com>
2024-01-06 12:28:51 +0000

    introduces PHPUnit test environment

---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Sarif-PHP-SDK package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Bartlett\Sarif\Tests;
+
+/**
+ * @author Laurent Laville
+ * @since Release 1.1.0
+ */
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+}
