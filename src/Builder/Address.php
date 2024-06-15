141d5397f3eb49f753ac2d930209627655c07ee6
Laurent Laville <laurent.laville@gmail.com>
2024-06-15 12:45:28 +0000

    add phpDoc blocs missing in recent release 1.5.0

--- src/Builder/Address.php
+++ src/Builder/Address.php
@@ -1,9 +1,18 @@
 <?php declare(strict_types=1);
-
+/**
+ * This file is part of the Sarif-PHP-SDK package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Bartlett\Sarif\Builder;
 
 use Bartlett\Sarif\Definition;
 
+/**
+ * @author Laurent Laville
+ * @since Release 1.5.0
+ */
 final class Address extends Declaration
 {
     protected int $absoluteAddress;
