b6862ef3183d6e11d466a3163cf755523bee7b9f
Laurent Laville <laurent.laville@gmail.com>
2026-04-20 06:10:22 +0000

    re-enable Graph Composer generation (compatible on PHP 8.2+ platform)

--- resources/gh-pages-hook.sh
+++ resources/gh-pages-hook.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ASSETS_IMAGE_DIR="docs/assets/images"
 
-#php $SCRIPT_DIR/build.php graph-composer $ASSETS_IMAGE_DIR
+php $SCRIPT_DIR/build.php graph-composer $ASSETS_IMAGE_DIR
 php $SCRIPT_DIR/build.php builder-api $ASSETS_IMAGE_DIR
 php $SCRIPT_DIR/build.php reference-address $ASSETS_IMAGE_DIR
 php $SCRIPT_DIR/build.php reference-artifact $ASSETS_IMAGE_DIR
